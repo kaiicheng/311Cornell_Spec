44568d7e9daf436f8a54ae07c7428f0c3c0341fb
[your <tw841117@gmail.com>
2023-12-01 16:09:20 -0500

    upload

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/metrics":["static\u002Fchunks\u002Fpages\u002Fmetrics.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fmetrics"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
